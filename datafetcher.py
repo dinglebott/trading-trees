7d15d7a70ef851f346039132a4160be1126d320a
yujay <tyujay@gmail.com>
2026-02-24 14:26:12 +0800

    add looping data fetcher

--- datafetcher.py
+++ datafetcher.py
@@ -2,48 +2,129 @@ import os
 from dotenv import load_dotenv
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+import time
 import pandas as pd
 
 # get oanda api key (from .env file)
 load_dotenv()
 apiKey = os.getenv("API_KEY")
 
-# FETCHING DATA (also saves locally as JSON file)
-def getData(instr="EUR_USD", gran="H1", count=500):
-    # granularity values: S5 S10 S15 S30, M1 M2 M4 M5 M10 M15 M30, H1 H2 H3 H4 H6 H8 H12, D W M
-    # max count: 5000
+# global variables
+headers = {"Authorization": f"Bearer {apiKey}"}
+baseUrl = "https://api-fxtrade.oanda.com" # access token generated from live account (don't use fxpractice)
 
+# NOTES AND EXPLANATION
+# instrument: EUR_USD, USD_JPY etc
+# granularity values: S5/10/15/30, M1/2/4/5/10/15/30, H1/2/3/4/6/8/12, D, W, M
+# count: default 500, maximum 5000
+# price: mid(M), bid(B), ask(A)
+# LOOP variant:
+# start, end in datetime format: e.g. datetime(2000, 1, 1) (year, month, day)
+# no count (defined by start/end and granularity)
+
+
+# FETCH DATA ONCE (also saves locally as JSON file)
+def getData(instr="EUR_USD", gran="H1", count=500):
     # get response
-    headers = {"Authorization": f"Bearer {apiKey}"}
     params = {
         "granularity": gran,
         "count": count,
         "price": "M"
     }
-    baseUrl = "https://api-fxtrade.oanda.com" # access token generated from live account (don't use fxpractice)
     endpoint = f"/v3/instruments/{instr}/candles"
     response = requests.get(baseUrl + endpoint, headers=headers, params=params)
     
     # inspect response
     if response.status_code != 200:
         raise Exception(response.text)
-    else:
-        print("Successful fetch")
     
     # save response
     data = response.json()
     directory = f"json_data/{instr}/{gran}"
     if not os.path.exists(directory):
         os.makedirs(directory)
-    timestamp = datetime.now().strftime("%m%d_%H%M") # extract month date hour minute from datetime
-    filename = f"{instr}_{gran}_{count}_{timestamp}.json"
+    timestamp = datetime.now().strftime("%m%d-%H%M") # extract month date hour minute from datetime
+    filename = f"{instr}_{gran}_{count}x_{timestamp}.json"
     filepath = os.path.join(directory, filename)
     with open(filepath, "w") as file:
         json.dump(data, file, indent=4)
+    print("Saved to: " + filename)
     
     # expose file path
     return filepath
+
+
+# FETCH DATA LOOP (also saves locally as JSON file)
+def getDataLoop(
+    start,
+    end,
+    instr="EUR_USD",
+    gran="H1"
+):
+    # get response
+    endpoint = f"/v3/instruments/{instr}/candles"
+    allCandles = []
+    currentStart = start
+
+    while currentStart < end:
+        params = {
+            "from": currentStart.isoformat() + "Z", # zero offset from UTC
+            "granularity": gran,
+            "count": 5000,
+            "price": "M"
+        }
+        response = requests.get(baseUrl + endpoint, headers=headers, params=params)
+
+        # inspect response
+        if response.status_code != 200:
+            raise Exception(response.text)
+        
+        # add response to final list
+        data = response.json()
+        candles = data["candles"]
+        if not candles:
+            print("Data not fetched")
+            break
+        allCandles += candles
+
+        # move start time forward
+        lastCandleTime = candles[-1]["time"] # in isoformat
+        match gran[0]: # get 1 candle worth of time in timedelta format
+            case "S":
+                timeAdvance = timedelta(seconds=int(gran[1:]))
+            case "M":
+                if len(gran) == 1:
+                    timeAdvance = timedelta(days=31)
+                else:
+                    timeAdvance = timedelta(minutes=int(gran[1:]))
+            case "H":
+                timeAdvance = timedelta(hours=int(gran[1:]))
+            case "D":
+                timeAdvance = timedelta(days=int(gran[1:]))
+            case "W":
+                timeAdvance = timedelta(weeks=int(gran[1:]))
+        
+        currentStart = datetime.strptime(
+            lastCandleTime.split(".")[0], # split to remove fractional seconds
+            "%Y-%m-%dT%H:%M:%S" # format
+        ) + timeAdvance # avoid duplicating last candle
+        print("Downloaded until: " + lastCandleTime)
+
+        # rate limit
+        time.sleep(0.5)
+    
+    # save to file
+    directory = f"json_data/{instr}/{gran}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    timestamp = datetime.now().strftime("%m%d-%H%M") # extract month date hour minute from datetime
+    filename = f"{instr}_{gran}_{start}-{end}_{timestamp}.json"
+    filepath = os.path.join(directory, filename)
+    with open(filepath, "w") as file:
+        json.dump({"candles": allCandles}, file, indent=4)
+    print("Saved to: " + filename)
+
 
 # PARSING DATA
 def parseData(jsonPath):
